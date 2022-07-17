a6b294ca2fc269ed80fac1d55fd2f833551f4978
Aditya <adityasureshkamath@gmail.com>
2022-07-17 19:26:12 +0530

    First code

---
+++ mongobd.py
@@ -0,0 +1,12 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Adk1411:Aditya123@ineuron.ppsex.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
+
